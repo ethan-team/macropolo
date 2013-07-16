dcbc2bff50bfa120d08fff1492e794aa5d424c64
jiangyizhen3 <ethan3.jiang@gmail.com>
2013-07-16 19:50:54 +0800

    rails new -sql -no-unit-test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Macropolo::Application.config.secret_token = 'c4ce6558cdcf2d98f8e97150bff2208c4c5b69292341e9c62f73218819fa712b5169f13dd1853148b7d2a596011f82872acb930026c3e4945e03ecb74daadc9d'
