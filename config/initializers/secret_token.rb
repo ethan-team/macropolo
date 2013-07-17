f34c9272a89c59d1d4cdee2f37b01f565b1f7c24
jiangyizhen3 <ethan3.jiang@gmail.com>
2013-07-17 12:55:09 +0800

    using devise-cancan-bootstrap rail generate to regenerate the application

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Macropolo::Application.config.secret_token = 'c4ce6558cdcf2d98f8e97150bff2208c4c5b69292341e9c62f73218819fa712b5169f13dd1853148b7d2a596011f82872acb930026c3e4945e03ecb74daadc9d'
+Macropolo::Application.config.secret_token = '2d1c8edb70d748cf9a190d0a80c5b7f145282cad9881908982364837dd99fd1b4c64d057bab27d80b64b6c019cfd8264b19cd85545850e86d03012de7b985e8e'
