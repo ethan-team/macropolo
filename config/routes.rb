551109c8d3323cd17497dd14ce37fa7380a85afe
jiangyizhen3 <ethan3.jiang@gmail.com>
2013-07-21 22:00:02 +0800

    fixing some routes issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Macropolo::Application.routes.draw do
 
-  resources :offers
+  #resources :offers  - do not want to expose the entire offer REST for now
+  get '/offers/:id(.:format)', :to => "offers#show", :as => :offer
 
+  #welcome to our home
   root :to => "home#index"
-  get  "/search", :to => "home#search" 
+
+  # this is our search result... keep it here (do not move to offers controller as we may implement offers REST)
   post "/search", :to => "home#search"
 
   ActiveAdmin.routes(self)
--- spec/controllers/offers_controller_spec.rb
+++ spec/controllers/offers_controller_spec.rb
@@ -4,7 +4,8 @@ describe OffersController do
 
   describe "GET 'show'" do
     it "returns http success" do
-      get 'show'
+      #get 'show'
+      get :show, :id => 0
       response.should be_success
     end
   end
